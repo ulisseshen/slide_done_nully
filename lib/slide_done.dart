985dcee364f7a3b3277816588d6955e99be129da
Ulisses Hen <ulisses.hen@hotmail.com>
2022-01-07 19:48:05 -0300

    atualizado referência.

--- lib/slide_done.dart
+++ lib/slide_done.dart
@@ -2,7 +2,9 @@
     Author: Qingyi
     Email: xuelongqy@foxmail.com
     createTime:2018-12-28 16:47
+    
+    Atualizado por Ulisses Hen  2022-01-07 19:47
  */
 library flutter_slide_done;
 
-export 'src/slide_done.dart';
+export 'src/slide_done_null_safety.dart';
